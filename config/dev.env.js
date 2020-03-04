9a7da472e23189673873d85126168aec10ef1cf2
binger <290169736@qq.com>
2020-03-04 15:26:42 +0800

    更改了接口请求路径

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,6 @@ const prodEnv = require("./prod.env");
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_ROOT: '"/public/cmsv4/"'
+  // API_ROOT: '"http://binger.natapp1.cc/public/cmsv4/"'
+  API_ROOT: '"http://api.howeaver.cn/public/cmsv4/"'
 });
